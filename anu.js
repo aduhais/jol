82e378fdbb2aa8e56c5504031d6ad3810aa99dec
aduhais <40473327+aduhais@users.noreply.github.com>
2018-06-22 01:55:30 +0700

    Add files via upload

---
+++ anu.js
@@ -0,0 +1,11 @@
+<div id="tivi"><iframe allowfullscreen="true" src="//www.bolamaniax.com/2018/01/bein-sport-1.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe> <script type="text/javascript">
+   function server1() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.bolamaniax.com/2018/01/bein-sport-0.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server2() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.bolamaniax.com/2018/01/bein-sport-2.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server3() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.bolamaniax.com/2018/01/bein-sport-3.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+</script></div><div id="tvtom"><a class="chtv" style="background:#CC0000" rel="nofollow" href="">Refresh</a><a class="chtv" href="javascript:server1();">Live 1</a><a class="chtv" href="javascript:server2();">Live 2</a><a class="chtv" href="javascript:server3();">Live 3</a></div>
